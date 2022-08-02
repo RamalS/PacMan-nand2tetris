301b17d0b7dfbac97f1f5f9e96103144f731e50b
rsalha <ramal.salha@gmail.com>
2022-08-02 18:48:16 +0200

    FEAT: Setup finished

---
+++ build.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+path = os.getcwd()
+build = os.path.join(path, "build")
+
+def MoveFiles(path):
+    for filename in os.listdir(path):
+        if filename.endswith(".jack"):
+            try:
+                shutil.copy(os.path.join(path, filename),
+                            os.path.join(build, filename))
+            except:
+                pass
+        elif os.path.isdir(os.path.join(path, filename)):
+            MoveFiles(os.path.join(path, filename))
+
+
+if __name__ == "__main__":
+    if os.path.exists(build):
+        for files in os.listdir(build):
+            os.remove(os.path.join(build, files))
+    else:
+        os.mkdir(build)
+
+    MoveFiles(path)
+
+    os.system(f'JackCompiler.bat "{build}"')
